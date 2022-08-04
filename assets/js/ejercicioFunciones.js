568841ca1788728ce0394dd6385fc8d21171eb5d
LucyVillasenor <lucia.auroravp@gmail.com>
2022-08-03 19:35:14 -0500

    Se añade ejercicio en equipo de de funciones

---
+++ assets/js/ejercicioFunciones.js
@@ -0,0 +1,23 @@
+
+/////EJERCICIO 1 ///////////
+nom= prompt("¿Cómo te llamas?");
+ubi= prompt("¿Donde vives?");
+pasaTiem= prompt("¿Cúal es tu pasatiempo?");
+
+function idCard(x,y,z){
+    nombre=x;
+    ubicacion=y;
+    pasaTiempo=z;
+    console.log(`Tu nombre es: ${nom} vives en: ${ubicacion} y tu pasatiempo es: ${pasaTiem} `);
+}
+
+idCard(nom,ubi,pasaTiem);
+
+
+/////EJERCICIO 2 ///////////
+edad= prompt("¿Cúal es la edad de tu husky?");
+function calcMascotAge(x){
+    edad=x * ;
+    console.log(`la edad humana de tu Husky es: ${edad} `)
+}
+calcMascotAge(edad);
